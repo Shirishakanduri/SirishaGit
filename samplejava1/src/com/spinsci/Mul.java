cc418ed6142ae57a08a5c903252e99a49de89657
Surya <surya1988@gmail.com>
2016-05-05 15:46:52 +0530

    new mehtod in mul.java

--- samplejava1/src/com/spinsci/Mul.java
+++ samplejava1/src/com/spinsci/Mul.java
@@ -8,6 +8,10 @@ public class Mul {
 		System.out.println("multiplication"+(a*b));
 		
 }
+	public String sayHelloAll2(){
+		System.out.println("haii to all");
+		return "welcome";
+	}
 	public static void main(String[] args) {
 		Mul m1=new Mul();
 		m1.mul();
