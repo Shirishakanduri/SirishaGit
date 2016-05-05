d35f358f51b812135dc4c37f5598f55752d3014c
unknown <Shirisha@spinsci_36.in.spinsci.com>
2016-05-05 15:09:26 +0530

    sample

---
+++ samplejava1/src/com/spinsci/Div.java
@@ -0,0 +1,13 @@
+package com.spinsci;
+
+public class Div {
+	void div(){
+		int a,b;
+		a=10;
+		b=20;
+		System.out.println("division"+(b/a));
+}
+	public static void main(String[] args) {
+		Div d=new Div();
+		d.div();
+	}}
---
+++ samplejava1/src/com/spinsci/Mul.java
@@ -0,0 +1,14 @@
+package com.spinsci;
+
+public class Mul {
+	void mul(){
+		int a,b;
+		a=10;
+		b=20;
+		System.out.println("multiplication"+(a*b));
+		
+}
+	public static void main(String[] args) {
+		Mul m1=new Mul();
+		m1.mul();
+	}}
