1bd2d0b9f54cac90dc65888a725b5ee40be01eae
Shirisha <Shirisha@spinsci_36.in.spinsci.com>
2016-05-06 15:18:08 +0530

    modify program for dependency

---
+++ Dependency/src/com/spin/TestDemo.java
@@ -0,0 +1,14 @@
+package com.spin;
+
+public class TestDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("sdfskjdfj");
+		System.err.println("sdfskjdfj");
+	}
+
+}
