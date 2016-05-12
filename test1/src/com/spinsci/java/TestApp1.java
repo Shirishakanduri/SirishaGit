07d07688ce22c84226fcfca7b03df0c25a74ef35
Shirisha <Shirisha@spinsci_36.in.spinsci.com>
2016-05-12 13:49:09 +0530

    jdjjdsdsd

--- test1/src/com/spinsci/java/TestApp1.java
+++ test1/src/com/spinsci/java/TestApp1.java
@@ -8,6 +8,7 @@ public class TestApp1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("this is test app");
+System.out.println("this is test app");
 	}
 
 }
